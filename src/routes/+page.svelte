ebea08243bcbaeae0b7078c8a7b5271d41a22598
Landon Noss <landongn@gmail.com>
2023-04-10 13:52:12 -0700

    tauri setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // Insert any necessary JavaScript here
+  </script>
+  
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+      <div class="text-center">
+        <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span class="block">Inspire Your Creativity</span>
+          <span class="block text-blue-600">with mob.social</span>
+        </h1>
+        <p class="mt-5 text-xl text-gray-500">
+          mob.social is the platform for creatives looking to take control of their content and connect with a like-minded community. With our decentralized and secure network, you can share your work, collaborate with peers, and discover inspiration in a new way.
+        </p>
+        <div class="mt-10">
+          <a href="#" class="text-base font-medium text-blue-600 hover:text-blue-500">Get Started</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="py-16 bg-gray-50 overflow-hidden">
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-center">
+        <p class="text-base leading-6 text-blue-600 font-semibold tracking-wide uppercase">Decentralized Network</p>
+        <h2 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+          Share Your Work Securely
+        </h2>
+        <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+          With our decentralized network, you can take control of your content and connect with a like-minded community without worrying about censorship or data privacy issues. Our secure platform ensures that your work is safe and your privacy is protected.
+        </p>
+      </div>
+  
+      <div class="mt-20">
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          <div class="pt-6">
+            <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div class="-mt-6">
+                <div>
+                  <span class="inline-flex items-center justify-center p-3 bg-blue-600 rounded-md shadow-lg">
+                    <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                  </span>
+                </div>
+                <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  Secure Communication
+                </h3>
+                <p class="mt-5 text-base text-gray-500">
+                  Communicate with other creatives and share your work securely, without worrying about surveillance or censorship.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div></div></div>
